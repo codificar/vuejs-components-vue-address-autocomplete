b0c5d5ce095f6a32e4eaf086296a7145a1a65fe3
gustavo.silva <gustavo.silva@codificar.com.br>
2020-10-02 17:25:02 -0300

    Use rollup to publish

---
+++ dev/serve.vue
@@ -0,0 +1,37 @@
+<script>
+import Vue from 'vue';
+import VueAddressAutocomplete from '@/vue-address-autocomplete.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueAddressAutocomplete
+  },
+  methods: {
+    getSelectedData(value) {	
+      console.log(value);
+    }
+	}
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-address-autocomplete 
+      ref="autocomplete"
+      PlaceHolderText="Escreva o endereço"
+      NeedAddressNumberText="Escreva o endereço"
+      :AutocompleteParams='{ 
+        "id": "",
+        "token": "",
+        "latitude": -19.743745,
+        "longitude": -43.8386481
+      }'            
+      AutocompleteUrl="teste.com"
+      GeocodeUrl='teste.com'
+      :MinLength='5'
+      :Delay='1000'
+      @addressSelected='getSelectedData'
+    />
+  </div>
+</template>
