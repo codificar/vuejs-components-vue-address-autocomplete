d9a89dd6dc9478cda4cbe5c3c96454d684c19eac
gustavo.silva <gustavo.silva@codificar.com.br>
2020-10-07 15:32:07 -0300

    Fix Packages Node Import

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,8 @@
 import async from 'rollup-plugin-async';
 import commonjs from 'rollup-plugin-commonjs'; // Converte módulos CommonJS para ES6
 import vue from 'rollup-plugin-vue'; // Manipula arquivos .vue
+import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
 // import buble from 'rollup-plugin-buble'; // Transpila com considerável suporte a navegadores
 export default {
   input: 'src/wrapper.js', // Caminho relativo ao package.json
@@ -15,6 +17,8 @@ export default {
       compileTemplate: true, // Explicitamente converte template para função render
     }),
     async(),
+    resolve({ jsnext: true, preferBuiltins: true, browser: true }),
+    json()
     // buble(), // Transpila para ES5
   ],
 };
